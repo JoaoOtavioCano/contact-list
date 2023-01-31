ed31659639e7d333532dd8959d09b59ba0572a94
Joao Otavio Cano <joaootaviocano@MacBook-Air-de-Joao.local>
2023-01-30 21:01:49 -0300

    Cleaned files

--- lista.h
+++ lista.h
@@ -6,29 +6,5 @@
 #define ERRO_CHEIA 0
 #define SUCESSO 1
 
-typedef char *t_chave;
-
-typedef struct t_elemento {
-  t_chave chave;
-  char *numero;
-} t_elemento;
-
-typedef struct t_no *t_apontador;
-
-typedef struct t_no {
-  t_elemento elemento;
-  t_apontador proximo;
-} t_no;
-
-typedef struct t_list {
-  t_apontador primeiro;
-} t_lista;
-
-int criar(t_lista *lista);
-int inserir(t_lista *lista, t_elemento elemento);
-int remover(t_lista *lista, t_chave chave);
-int pesquisar(t_lista *lista, t_chave chave);
-int alterar(t_lista *lista, t_elemento novo_elemento);
-static t_apontador pesquisa_pos(t_lista *lista, t_chave chave);
 
 #endif
--- main.c
+++ main.c
@@ -4,62 +4,6 @@
 #include "lista.h"
 
 int main(){
-  t_lista l;
-  t_elemento contato;
-  char operacao;
-  int i = 0;
-  t_chave nome[10], numero[9];
   
-
-  criar(&l);
-
-  contato.chave = (t_chave)malloc(10*sizeof(t_chave));
-  contato.numero = (t_chave)malloc(9*sizeof(t_chave));
-
-  
-  while(i < 1000000000 ){
-  scanf("%c", &operacao);
-
-
-    switch (operacao) {
-      case 'I':
-      
-        scanf("%s", &nome);
-        scanf("%s", &numero);
-        strcpy(contato.chave, nome);
-        strcpy(contato.numero, numero);
-        inserir(&l, contato);
-        i++;
-        break;
-      
-      case 'R':
-        scanf("%s", &nome);
-        strcpy(contato.chave, nome);
-        scanf("%s", contato.chave);
-        remover(&l, contato.chave);
-        i++;
-        break;
-      
-      case 'A':
-        scanf("%s", &nome);
-        scanf("%s", &numero);
-        strcpy(contato.chave, nome);
-        strcpy(contato.numero, numero);
-        alterar(&l, contato);
-        i++;
-        break;
-      
-      case 'P':
-        scanf("%s", &nome);
-        strcpy(contato.chave, nome);
-        pesquisar(&l, contato.chave);
-        i++;
-        break;
-
-      case '0':
-        exit(1);
-        break;
-    }
-  }
     return 0;
 }
